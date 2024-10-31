dc724bc22ecb95337b725664a1bb3549797770a6
Leon Afanasenko <afanasenko.leon@gmail.com>
2024-10-31 13:24:40 +0100

    Creating and deleting a directory

---
+++ work_task_fsjob_1/task2.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+fs.writeFile("info.txt", "Node.js is awesome!", (err) => {
+  if (err) {
+    return console.error(`Ошибка при записи в файл: ${err.message}`);
+  }
+  console.log("Текст успешно записан в файл info.txt.");
+
+  fs.readFile("info.txt", "utf8", (err, data) => {
+    if (err) {
+      return console.error(`Ошибка при чтении файла: ${err.message}`);
+    }
+    console.log("Содержимое файла info.txt:");
+    console.log(data);
+  });
+});
