600a962a3f9edfd1bee7aaee0e7f215e72dddad4
Leon Afanasenko <afanasenko.leon@gmail.com>
2024-11-18 11:53:31 +0100

    chore: implementation of JWT authentication, route protection, and API testing

---
+++ jwt-protected-routes/authorizeRole.js
@@ -0,0 +1,15 @@
+const authorizeRole = (role) => {
+  return (req, res, next) => {
+    if (req.user.role === role) {
+      next();
+    } else {
+      res
+        .status(403)
+        .json({
+          message: "Forbidden: You do not have access to this resource",
+        });
+    }
+  };
+};
+
+module.exports = authorizeRole;
---
+++ jwt-protected-routes/users.js
@@ -0,0 +1,11 @@
+const users = [
+  {
+    id: 1,
+    username: "leon",
+    email: "leon@example.com",
+    password: "$2b$10$ViA8fPyduGnXdN9V3tb72e1q7YAGoXa4yqWSXJKE8Xl9VqoKpt4Xa",
+    role: "admin",
+  },
+];
+
+module.exports = users;
