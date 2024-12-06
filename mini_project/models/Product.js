63764d9295379ab8efba56a6207d40eeccc05b90
Leon Afanasenko <afanasenko.leon@gmail.com>
2024-12-06 09:30:28 +0100

    mini-project

---
+++ mini_project/models/Product.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
